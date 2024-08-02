75c25b931cd72d934aa5fbb35280543a93e3ce29
fatima-harun <faabintu@gmail.com>
2024-08-02 13:39:15 +0000

    ajout des fichiers

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Article::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' =>'required|string|max:255',
+            'body' => 'required|string',
+        ]);
+        return Article::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $article = Article::find($id);
+
+        if(!$article){
+            return response()->json(['message' => 'article non trouvé'],404);
+        }
+        return $article;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
